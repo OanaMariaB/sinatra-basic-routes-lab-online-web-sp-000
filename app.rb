04a2815730ac8f8c494014eaabef2ea02bb03a35
Oana Berentan <oana.berentan@yahoo.com>
2019-12-01 15:15:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-  "My name is "
-end
+    "My name is "
+  end
+
+  get '/hometown' do
+    "My hometown is "
+  end
+  
 end
